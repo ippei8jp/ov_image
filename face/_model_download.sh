7ffe78c004932c42aedbbb27cdae35f8e3b78dd4
いっぺーちゃん <48367499+ippei8jp@users.noreply.github.com>
2020-10-17 06:28:46 +0900

    不要なコメント削除

--- face/_model_download.sh
+++ face/_model_download.sh
@@ -17,23 +17,3 @@ function download_model_zoo () {
 download_model_zoo	"face-detection-retail-0004"
 download_model_zoo 	"landmarks-regression-retail-0009"
 download_model_zoo	"head-pose-estimation-adas-0001"
-
-
-
-# MODELS_DIR=models
-# mkdir -p ${MODELS_DIR}
-# 
-# BASE_URL="https://download.01.org/opencv/2020/openvinotoolkit/2020.3/open_model_zoo/models_bin/1"
-# 
-# 
-# MODEL_NAME="face-detection-retail-0004"
-# wget --no-check-certificate -P ${MODELS_DIR} ${BASE_URL}/${MODEL_NAME}/FP16/${MODEL_NAME}.bin
-# wget --no-check-certificate -P ${MODELS_DIR} ${BASE_URL}/${MODEL_NAME}/FP16/${MODEL_NAME}.xml
-# 
-# MODEL_NAME="landmarks-regression-retail-0009"
-# wget --no-check-certificate -P ${MODELS_DIR} ${BASE_URL}/${MODEL_NAME}/FP16/${MODEL_NAME}.bin
-# wget --no-check-certificate -P ${MODELS_DIR} ${BASE_URL}/${MODEL_NAME}/FP16/${MODEL_NAME}.xml
-# 
-# MODEL_NAME="head-pose-estimation-adas-0001"
-# wget --no-check-certificate -P ${MODELS_DIR} ${BASE_URL}/${MODEL_NAME}/FP16/${MODEL_NAME}.bin
-# wget --no-check-certificate -P ${MODELS_DIR} ${BASE_URL}/${MODEL_NAME}/FP16/${MODEL_NAME}.xml
