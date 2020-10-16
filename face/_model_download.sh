2444ea4e93c4d5d4e8fdbd1ec570146f59b886ec
いっぺーちゃん <48367499+ippei8jp@users.noreply.github.com>
2020-10-16 15:07:10 +0900

    処理全面書き換え

--- face/_model_download.sh
+++ face/_model_download.sh
@@ -1,17 +1,39 @@
 MODELS_DIR=models
+MODELS_DIR_ABS=`realpath ${MODELS_DIR}`
 mkdir -p ${MODELS_DIR}
 
-BASE_URL="https://download.01.org/opencv/2020/openvinotoolkit/2020.3/open_model_zoo/models_bin/1"
+# MODEL ZOOからのダウンロード ##################################################################
+function download_model_zoo () {
+# 	local BASE_URL="https://download.01.org/opencv/2020/openvinotoolkit/2020.3/open_model_zoo/models_bin/1"
+	local BASE_URL="https://download.01.org/opencv/2021/openvinotoolkit/2021.1/open_model_zoo/models_bin/1/"
+	
+	local MODEL_NAME=$1
+	
+	wget --no-check-certificate -O ${MODELS_DIR}/${MODEL_NAME}.bin ${BASE_URL}/${MODEL_NAME}/FP16/${MODEL_NAME}.bin
+	wget --no-check-certificate -O ${MODELS_DIR}/${MODEL_NAME}.xml ${BASE_URL}/${MODEL_NAME}/FP16/${MODEL_NAME}.xml
+	
+}
+
+download_model_zoo	"face-detection-retail-0004"
+download_model_zoo 	"landmarks-regression-retail-0009"
+download_model_zoo	"head-pose-estimation-adas-0001"
 
 
-MODEL_NAME="face-detection-retail-0004"
-wget --no-check-certificate -P ${MODELS_DIR} ${BASE_URL}/${MODEL_NAME}/FP16/${MODEL_NAME}.bin
-wget --no-check-certificate -P ${MODELS_DIR} ${BASE_URL}/${MODEL_NAME}/FP16/${MODEL_NAME}.xml
 
-MODEL_NAME="landmarks-regression-retail-0009"
-wget --no-check-certificate -P ${MODELS_DIR} ${BASE_URL}/${MODEL_NAME}/FP16/${MODEL_NAME}.bin
-wget --no-check-certificate -P ${MODELS_DIR} ${BASE_URL}/${MODEL_NAME}/FP16/${MODEL_NAME}.xml
-
-MODEL_NAME="head-pose-estimation-adas-0001"
-wget --no-check-certificate -P ${MODELS_DIR} ${BASE_URL}/${MODEL_NAME}/FP16/${MODEL_NAME}.bin
-wget --no-check-certificate -P ${MODELS_DIR} ${BASE_URL}/${MODEL_NAME}/FP16/${MODEL_NAME}.xml
+# MODELS_DIR=models
+# mkdir -p ${MODELS_DIR}
+# 
+# BASE_URL="https://download.01.org/opencv/2020/openvinotoolkit/2020.3/open_model_zoo/models_bin/1"
+# 
+# 
+# MODEL_NAME="face-detection-retail-0004"
+# wget --no-check-certificate -P ${MODELS_DIR} ${BASE_URL}/${MODEL_NAME}/FP16/${MODEL_NAME}.bin
+# wget --no-check-certificate -P ${MODELS_DIR} ${BASE_URL}/${MODEL_NAME}/FP16/${MODEL_NAME}.xml
+# 
+# MODEL_NAME="landmarks-regression-retail-0009"
+# wget --no-check-certificate -P ${MODELS_DIR} ${BASE_URL}/${MODEL_NAME}/FP16/${MODEL_NAME}.bin
+# wget --no-check-certificate -P ${MODELS_DIR} ${BASE_URL}/${MODEL_NAME}/FP16/${MODEL_NAME}.xml
+# 
+# MODEL_NAME="head-pose-estimation-adas-0001"
+# wget --no-check-certificate -P ${MODELS_DIR} ${BASE_URL}/${MODEL_NAME}/FP16/${MODEL_NAME}.bin
+# wget --no-check-certificate -P ${MODELS_DIR} ${BASE_URL}/${MODEL_NAME}/FP16/${MODEL_NAME}.xml
